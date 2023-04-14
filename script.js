fd4a1ad28c1e1b64e9fd831281f05820a95cf509
Alexandra <alexandra.bazir@gmail.com>
2023-04-14 13:34:50 +0300

    Добавлено редактирование карточек

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ function createCard(cat, el = container) {
     })
     const trash = document.createElement("i");
     trash.className = "fa-solid fa-trash-can card__trash";
+    trash.title = "Удалить котика =(";
     trash.addEventListener("click", e => {
         e.stopPropagation();
         deleteCard(cat.id, e.currentTarget.parentElement);
@@ -47,63 +48,67 @@ function createCard(cat, el = container) {
         modalInfo.style.display = "block";
         showCard(cat.id);
     })
-        card.append(like, name, trash, info);
-        if (cat.age >= 0) {
-            const age = document.createElement("span");
-            age.innerText = cat.age;
-            card.append(age);
-        }
-        el.append(card);
+    const edit = document.createElement("i");
+    edit.className = "fa-solid fa-pencil card__edit";
+    edit.title = "Редактировать котика";
+    edit.addEventListener("click", e => {
+        e.stopPropagation();
+        modalEdit.style.display = "block";
+        editCard(cat.id);
+    })
+    card.append(like, name, trash, info, edit);
+    if (typeof cat.age === "number") {
+        const age = document.createElement("span");
+        age.innerText = setAge(cat.age);
+        card.append(age);
+    }
+    el.append(card);
 }
 
 function deleteCard(id, el) {
-            if (id) {
-                fetch(`${path}/delete/${id}`, {
-                    method: "delete"
-                })
-                    .then(res => {
-                        if (res.status === 200) {
-                            el.remove();
-                        }
-                    })
-            }
-        }
+    if (id) {
+        fetch(`${path}/delete/${id}`, {
+            method: "delete"
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    el.remove();
+                }
+            })
+    }
+}
 
 fetch(path + "/show")
-            .then(function (res) {
-                if (res.statusText === "OK") {
-                    return res.json();
-                }
-            })
-            .then(function (data) {
-                if (!data.length) {
-                    container.innerHTML = "<div class=\"empty\">У вас пока еще нет питомцев</div>"
-                } else {
-                    for (let c of data) {
-                        createCard(c, container);
-                    }
-                }
-            })
+    .then(function (res) {
+        if (res.statusText === "OK") {
+            return res.json();
+        }
+    })
+    .then(function (data) {
+        if (!data.length) {
+            container.innerHTML = "<div class=\"empty\">У вас пока еще нет питомцев</div>"
+        } else {
+            for (let c of data) {
+                createCard(c, container);
+            }
+        }
+    })
 
 const modalBox = document.querySelector(".modal-container");
 const addButton = document.querySelector(".add-button");
-const modalClose = modalBox.querySelector(".modal-close");
+const closeAdd = modalBox.querySelector(".close-add");
 const addForm = document.forms.add;
 
 addButton.addEventListener("click", e => {
     modalBox.style.display = "flex";
 })
 
-modalClose.addEventListener("click", e => {
+closeAdd.addEventListener("click", e => {
     modalBox.style = null;
 })
 
 addForm.addEventListener("submit", e => {
-    e.preventDefault(); // остановить действие по умолчанию
     const body = {};
-    console.log(addForm.children); // дочерние теги (прямые потомки)
-    console.log(addForm.elements); // все элементы формы (input и т.д.)
-
     for (let i = 0; i < addForm.elements.length; i++) {
         const inp = addForm.elements[i];
         if (inp.name) {
@@ -137,7 +142,9 @@ addForm.addEventListener("submit", e => {
         });
 })
 
-    // коммент
+let modalInfo = document.querySelector(".modal-info");
+let modalContent = document.querySelector(".modal-content");
+let closeInfo = modalInfo.querySelector(".close-info");
 
 function showCard(id) {
     if (id) {
@@ -153,27 +160,15 @@ function showCard(id) {
                 <h3>${typeof data.age === "number" ? setAge(data.age) : "Возраст не указан"}</h3>
                 <p>${data.description || "Информации о котике пока нет..."}</p>
                 <p class="rate">${setRate(data.rate)}</p>
-            <img src=${data.image || "img/cat3.png"} alt="${data.name}" class="modal-img">
-            `;  
+            <img src=${data.image || "img/cats.png"} alt="${data.name}" class="modal-img">
+            `;
             })
     }
 }
 
-let modalInfo = document.querySelector(".modal-info");
-let modalContent = document.querySelector(".modal-content");
-let closeInfo = modalInfo.querySelector(".close-info");
-
 closeInfo.onclick = function (event) {
     event.stopImmediatePropagation();
-    console.log(event.currentTarget);
     modalInfo.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target == modalInfo) {
-        console.log(event.currentTarget);
-        modalInfo.style.display = "none";
-    }
 }
 
 function setRate(n) {
@@ -198,4 +193,89 @@ function setAge(n) {
     return n + " лет";
 }
 
-    
+let modalEdit = document.querySelector(".modal-edit");
+let modalEditContent = document.querySelector(".modal-edit-content");
+let closeEdit = modalEdit.querySelector(".close-edit");
+
+function editCard(id) {
+    if (id) {
+        fetch(`${path}/show/${id}`)
+            .then(function (res) {
+                if (res.statusText === "OK") {
+                    return res.json();
+                }
+            })
+            .then(function (data) {
+                modalEditContent.lastElementChild.innerHTML = `<h2>Изменить котика</h2>
+                <label for="upd1">id котика</label>
+                <input type="number" id="upd1" name="id" value="${data.id}" readonly>
+                <label for="upd2">Имя котика</label>
+                <input type="text" id="upd2" name="name" value="${data.name}" required>
+                <label for="upd3">Возраст</label>
+                <input type="number" id="upd3" name="age" value="${data.age}">
+                <label for="upd4">Изображение</label>
+                <input type="url" name="image" id="upd4" value="${data.image || ""}">  
+                <label for="upd5">Рейтинг</label>
+                <input type="number" name="rate" id="upd5" value="${data.rate}" min="0" max="5">
+                <label>
+                <input type="checkbox" name="favorite" ${data.favorite ? "checked" : ""}>
+                Любимчик</label>
+                <label for="upd7">Описание</label>
+                <textarea name="description" id="upd7">${data.description || ""}</textarea>
+                <button type="submit">Обновить</button>`;
+            })
+    }
+}
+
+closeEdit.onclick = function (event) {
+    event.stopImmediatePropagation();
+    modalEdit.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modalEdit || event.target == modalInfo || event.target == modalBox) {
+        modalEdit.style.display = "none";
+        modalInfo.style.display = "none";
+        modalBox.style.display = "none";
+    }
+}
+
+const updateForm = document.forms.update;
+
+
+updateForm.addEventListener("submit", e => {
+    const body = {};
+    for (let i = 0; i < updateForm.elements.length; i++) {
+        const inp = updateForm.elements[i];
+        if (inp.name) {
+            if (inp.type === "checkbox") {
+                body[inp.name] = inp.checked
+            } else {
+                body[inp.name] = inp.value
+            }
+        }
+    }
+    console.log("upd", body);
+    fetch(`${path}/update/${body.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+        .then(res => {
+            if (res.ok) {
+                updateForm.reset();
+                modalEdit.style = null;
+
+
+            } else {
+                return res.json();
+            }
+        })
+        .then(err => {
+            if (err && err.message) {
+                alert(err.message);
+            }
+        });
+})
